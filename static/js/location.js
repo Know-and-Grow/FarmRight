07a0579c4e7ca81e7807adac4a5e104bb351e6b2
Sakshi-25 <sakshijangra111000@gmail.com>
2022-03-08 20:03:54 +0530

    Initialising project website structure

---
+++ static/js/location.js
@@ -0,0 +1,7 @@
+var location =  [
+    ["Delhi", "South Delhi", "North Delhi", "East Delhi"],
+    ["Haryana", "Faridabad", "Jhajjar", "Agra", "Rohtak"],
+    ["Chandigarh", "New Town", "Distrct 1"]
+];
+
+var dd = new Dropdown(location)
