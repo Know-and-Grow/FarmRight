1259eb2ab379550783d1d8bf6d9126632f3eccec
dvitiarora <dvitiarora125btcse18@igdtuw.ac.in>
2022-05-02 16:56:12 +0530

    Added backend for chatbot

--- app.py
+++ app.py
@@ -8,6 +8,20 @@ import requests
 import config
 import requests
 import pickle
+import random
+import re
+
+
+import nltk
+try:
+    nltk.data.find('tokenizers/popular')
+except LookupError:
+    nltk.download('popular')
+
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+
+from keras.models import load_model
 
 # -------------------------LOADING THE TRAINED MODELS -----------------------------------------------
 
@@ -18,6 +32,10 @@ import pickle
 crop_recommendation_model_path = 'models/model.pkl'
 crop_recommendation_model = pickle.load(
     open(crop_recommendation_model_path, 'rb'))
+
+# Loading chatbot model
+
+chatbot_model = load_model('models/model.h5')
 
 
 
@@ -47,8 +65,74 @@ def weather_fetch(city_name):
     else:
         return None
 
+intents = json.loads(open('utils/intents.json').read())
+words = pickle.load(open('utils/texts.pkl','rb'))
+classes = pickle.load(open('utils/labels.pkl','rb'))
+
+def clean_up_sentence(sentence):
+    # tokenize the pattern - split words into array
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word - create short form for word
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+
+def bow(sentence, words, show_details=True):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)  
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s: 
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+    return(np.array(bag))
+
+def predict_class(sentence, model):
+    # filter out predictions below a threshold
+    p = bow(sentence, words,show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({"intent": classes[r[0]], "probability": str(r[1])})
+    return return_list
+
+def getResponse(ints, intents_json):
+    tag = ints[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if(i['tag']== tag):
+            result = random.choice(i['responses'])
+            break
+    return result
+
+def chatbot_response(msg):
+    ints = predict_class(msg, chatbot_model)
+    res = getResponse(ints, intents)
+    return res
 
 
+def findName(string):
+  length = len(string)
+  newstring=""
+  # traversing from last
+  for i in range(length-1, 0, -1):
+      
+      # if space is occurred then return
+      if(string[i] == " "):
+          
+          # return reverse of newstring
+          return newstring[::-1]
+      else:
+          newstring = newstring + string[i]
 # ===============================================================================================
 # ------------------------------------ FLASK APP -------------------------------------------------
 
@@ -63,7 +147,17 @@ def home():
     title = 'Know and Grow - Home'
     return render_template('index.html')
 
-# render crop recommendation form page
+@ app.route('/get',methods=['GET'])
+def get_bot_response():
+    
+    userText = request.args.get('msg')
+    if (bool(re.match("i am",userText.lower()))):
+        n=findName(userText)
+        return chatbot_response(userText).format(n=n)
+    else:
+        return chatbot_response(userText)
+    
+
 
 
 
@@ -129,3 +223,8 @@ def crop_prediction():
 # ===============================================================================================
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
--- static/js/chatbot_custom.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $('#icon').click(function(event){
-        $('.chatbot_box').toggleClass('active');
-    });
-
-    $('.conv-form-wrapper').convform({selectInputStyle: 'disable'});
-})
